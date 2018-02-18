11045bb35d6d9e0e76af7803a09321c8a37e6a38
Nitin Pradeep Kumar <getnpk@gmail.com>
2018-02-18 14:28:12 +0800

    Add header search component

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
 
+import Search from '../containers/search';
+
 class App extends Component {
   render() {
     return (
       <div className="App">
-        App
+        <Search />
       </div>
     );
   }
---
+++ src/containers/search.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class Search extends Component {
+  render() {
+    return (
+      <div className="main-search">
+        <form>
+          <input type="text" />
+        </form>
+      </div>
+    );
+  }
+}
+
+export default Search;
