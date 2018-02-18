106069268d489fd5a3eaa24cdd60038e9d89e70e
Nitin Pradeep Kumar <getnpk@gmail.com>
2018-02-18 14:36:31 +0800

    Flesh out search component

--- src/containers/search.js
+++ src/containers/search.js
@@ -1,11 +1,37 @@
 import React, { Component } from 'react';
 
 class Search extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      keyword: ''
+    };
+  }
+
+  handleChange = (event) => {
+    this.setState({
+      keyword: event.target.value
+    });
+  }
+
+  searchCars = (event) => {
+    event.preventDefault();
+  }
+
+  componentDidMount() {
+
+  }
+
   render() {
     return (
       <div className="main-search">
-        <form>
-          <input type="text" />
+        <form onSubmit={this.searchCars}>
+          <input
+            type="text"
+            onChange={(event) => this.handleChange(event)}
+            value={this.state.keyword}
+            />
         </form>
       </div>
     );
